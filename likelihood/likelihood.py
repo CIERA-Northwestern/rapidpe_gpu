9debc61ed7d7d19434b0ae96cadc9fd212b0d6e8
MILLER Brandon <bmu660@quser11.quest.it.northwestern.edu>
2016-08-04 17:39:36 -0500

    implemented F's

--- likelihood/likelihood.py
+++ likelihood/likelihood.py
@@ -23,6 +23,7 @@ max_tpb = 512
 nsamps = np.array(10240, ndmin=1).astype(np.int32)
 theta  = np.linspace(0, 2*math.pi, nsamps)
 phi    = np.linspace(0, 2*math.pi, nsamps)
+psi    = np.linspace(0, 2*math.pi, nsamps)
 
 # There will be a different function for
 # each l value. The m values will be handed
@@ -38,10 +39,12 @@ mlist_gpu = cuda.mem_alloc(mlist_sort.nbytes)
 
 theta_gpu = cuda.mem_alloc(theta.nbytes)
 phi_gpu   = cuda.mem_alloc(phi.nbytes)
+psi_gpu   = cuda.mem_alloc(phi.nbytes)
 
 cuda.memcpy_htod(mlist_gpu, mlist_sort)
 cuda.memcpy_htod(theta_gpu, theta)
 cuda.memcpy_htod(phi_gpu, phi)
+cuda.memcpy_htod(psi_gpu, psi)
 
 # We will need the number of modes as an int 
 # on the device for a loop within the kernel
@@ -61,7 +64,6 @@ cuda.memcpy_htod(nsamps_gpu, nsamps)
 # the necessary double prec. complex numbers
 
 spharms_l_eq_2     = np.zeros(nmodes*nsamps).astype(np.complex128)
-spharms_l_eq_2_gpu = cuda.mem_alloc(theta.nbytes*len(mlist_sort) * 2)
 
 # Length of the time series
 
@@ -144,6 +146,9 @@ __device__ int get_padded_idx_2d_1d(int ntimes, int ncols) {
         return threadIdx.x + blockIdx.y*ncols + ntimes; 
 }
 
+__device__ int get_global_idx_1d_1d() {
+	return blockIdx.x * blockDim.x + threadIdx.x;
+}
 
 // ROW MAJOR FORM
 __device__ int get_global_idx_2d_1d() {
@@ -225,6 +230,38 @@ __global__ void accordion(cuDoubleComplex *contr_arr, int *nmodes, int *ntimes, 
 } 
 
 
+__global__ void complex_antenna_factor(double *R, double *ra, double *dec, double *psi, double *tref, cuDoubleComplex *result) {
+	
+	extern __shared__ double Shr[];	
+
+	int gid = get_global_idx_1d_1d();	
+	int id  = threadIdx.x;
+
+	double *X = &Shr[(id * 6)];
+	double *Y = &Shr[(id * 6) + 3];
+
+	double gha = tref[gid] - ra[gid]; 
+	X[0] = -cos(psi[gid]) * sin(gha) - sin(psi[gid]) * cos(gha) * sin(dec[gid]);  
+	X[1] = -cos(psi[gid]) * cos(gha) + sin(psi[gid]) * sin(gha) * sin(dec[gid]);
+	X[2] =  sin(psi[gid]) * cos(dec[gid]);
+	Y[0] =  sin(psi[gid]) * sin(gha) - cos(psi[gid]) * cos(gha) * sin(dec[gid]); 
+	Y[1] =  sin(psi[gid]) * cos(gha) + cos(psi[gid]) * sin(gha) * sin(dec[gid]);
+	Y[2] =  cos(psi[gid]) * cos(dec[gid]);	
+
+	__syncthreads();
+	
+	double Fp;
+	double Fc;
+
+
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			Fp += X[i]*R[i*3 + j]*X[j] - Y[i]*R[i*3 + j]*Y[j]; 
+			Fc += Y[i]*R[i*3 + j]*X[j] + X[i]*R[i*3 + j]*Y[j]; 
+		}
+	}
+	result[gid] = make_cuDoubleComplex(Fp, Fc);
+}
 ''')
 
 
@@ -243,24 +280,15 @@ def compute_spharms_l_eq_2(spharm_getter, th_gpu, ph_gpu, selected_modes_gpu, nm
 	grd = (nblocks, 1, 1) # use 1D grid		
 	blk = (max_tpb, 1, 1) # maximum linear #threads
 	##### KERNEL LAUNCH
-	
-	#empty = mod.get_function("empty")
-	#empty(grid=grd, block=blk)
 
 	spharm_getter(th_gpu, ph_gpu, selected_modes_gpu, nmds_gpu, nsmps_gpu, rslt_gpu, block=blk, grid=grd)
 	##### 	
 	return rslt_gpu 
 
+spharms_l_eq_2_gpu = gpuarray.to_gpu(spharms_l_eq_2)
 
-#spharms_l_eq_2_gpu = compute_spharms_l_eq_2(_get_spharms_l_eq_2, theta_gpu, phi_gpu, mlist_gpu, nmodes_gpu, nsamps_gpu, spharms_l_eq_2_gpu)
+spharms_l_eq_2_gpu = compute_spharms_l_eq_2(_get_spharms_l_eq_2, theta_gpu, phi_gpu, mlist_gpu, nmodes_gpu, nsamps_gpu, spharms_l_eq_2_gpu)
 
-#_____________________
-# FIXME -For Debugging 
-#_____________________
-
-
-#cuda.memcpy_dtoh(spharms_l_eq_2, spharms_l_eq_2_gpu)
-print(spharms_l_eq_2[0:9])
 
 '''
  As of yet we have an (nmodes x nsamps) array of double
@@ -284,6 +312,39 @@ print(spharms_l_eq_2[0:9])
 
 '''
 
+#_____________________
+# Multiply in the F's  
+#_____________________
+
+R = np.array([-0.3926141 , -0.07761341, -0.24738905, # This thing is the  
+              -0.07761341,  0.31952408,  0.22799784, # detector tensor
+              -0.24738905,  0.22799784,  0.07309003]).astype(np.float64)
+
+R_gpu = gpuarray.to_gpu(R)
+
+complex_antenna_factor = np.zeros(nsamps).astype(np.complex128)
+complex_antenna_factor_gpu = gpuarray.to_gpu(complex_antenna_factor)
+
+tref = np.array([24715.581890875823 for item in theta]).astype(np.float64)
+tref_gpu = gpuarray.to_gpu(tref)
+
+def compute_complex_antenna_factor(antenna_fac_computer, det_tensor, ra, dec, psi, tref, result):
+	griddimx = int(nsamps[0] / max_tpb)	
+	grd = (griddimx, 1, 1)
+	blk = (max_tpb,  1, 1)
+	total_smem = max_tpb*8*6
+
+	antenna_fac_computer(det_tensor, ra, dec, psi, tref, result, grid=grd, block=blk, shared=(total_smem))
+
+	return result
+
+_compute_complex_antenna_factor = mod.get_function("complex_antenna_factor")
+complex_antenna_factor_gpu = compute_complex_antenna_factor(_compute_complex_antenna_factor, R_gpu, theta_gpu, phi_gpu, psi_gpu, tref_gpu, complex_antenna_factor_gpu) 
+
+for i in range(0, nmodes):
+	ifirst = i*nsamps
+	ilast  = (i+1)*nsamps
+	spharms_l_eq_2_gpu[ifirst:ilast] = spharms_l_eq_2_gpu[ifirst:ilast]*complex_antenna_factor_gpu
 
 #_____________________
 # build likelihood 
@@ -342,16 +403,13 @@ print(rhoTS[:, 0:10])
 rhoTS_gpu = cuda.mem_alloc(rhoTS.nbytes)
 cuda.memcpy_htod(rhoTS_gpu, rhoTS)
 
-_get_rhoTS_expander   = mod.get_function("expand_rhoTS")
-
-
 # We might not need this thanks to GPUarrays
-_get_pad_with_zeros = mod.get_function("pad_with_zeros")
 
+_get_rhoTS_expander = mod.get_function("expand_rhoTS")
 
 # Above two functions naturally group together
 # FIXME - should probably be combined into one
-def expand_rhoTS(rhoTS_expander, zero_padder, ncols_gpu, nmodes_gpu, ntimes_gpu, rhoTS_gpu, expansion_arr):
+def expand_rhoTS(rhoTS_expander, ncols_gpu, nmodes_gpu, ntimes_gpu, rhoTS_gpu, expansion_arr):
 	griddimx  = int(ncols[0] / max_tpb)	
 	griddimy  = int(nsamps[0]*nmodes[0])
 
@@ -360,13 +418,10 @@ def expand_rhoTS(rhoTS_expander, zero_padder, ncols_gpu, nmodes_gpu, ntimes_gpu,
 
 	rhoTS_expander(ncols_gpu, nmodes_gpu, ntimes_gpu, rhoTS_gpu, expansion_arr, grid=grd, block=blk)
 
-	# I don't think this is needed anymore
-	#zero_padder(pre_likelihood_gpu, ntimes_gpu, nsamps_gpu, ncols_gpu, padwidth_gpu, grid=(1, griddimy, 1), block=blk)
-
 	return expansion_arr
 
 
-rhoTS_expansion_gpu = expand_rhoTS(_get_rhoTS_expander, _get_pad_with_zeros, ncols_gpu, nmodes_gpu, ntimes_gpu, rhoTS_gpu, pre_likelihood_gpu) 
+rhoTS_expansion_gpu = expand_rhoTS(_get_rhoTS_expander, ncols_gpu, nmodes_gpu, ntimes_gpu, rhoTS_gpu, pre_likelihood_gpu) 
 
 #_____________________
 # Multiply in Ylms 
@@ -407,17 +462,6 @@ def ylms_into_rhoTS(ylm_multiplier, expanded_rhoTS, all_ylms, nmodes_gpu, ncols_
 	ylm_multiplier(expanded_rhoTS, all_ylms, nmodes_gpu, ncols_gpu, nsamps_gpu, grid=grd, block=blk, shared=(int(nmodes[0] *16)))
 
 	return expanded_rhoTS
-
-#_____________________
-# Expanded Ylm Test 
-#_____________________
-
-spharms_l_eq_2 = np.zeros((nmodes, nsamps)).astype(np.complex128)
-for i in range(nmodes):
-	spharms_l_eq_2[i,:] = np.arange(nsamps)
-
-
-spharms_l_eq_2_gpu = gpuarray.to_gpu(spharms_l_eq_2)
 
 all_rhots_ylm = ylms_into_rhoTS(_multiply_in_ylms, rhoTS_expansion_gpu, spharms_l_eq_2_gpu, nmodes_gpu, ncols_gpu, nsamps_gpu)
 
@@ -464,6 +508,12 @@ accordion = mod.get_function("accordion")
 accordion(all_rhots_ylm, nmodes_gpu, ntimes_gpu, ncols_gpu, grid=((int(ncols[0] / max_tpb)), int(nsamps[0]), 1), block=(max_tpb, 1, 1))
 
 
+
+'''
+ We need to get the maximums of the rows
+'''
+
+
 '''
  To get the likelihoods for all the samples, we will need to fold 
  the block of time series up along the sample axis with reduction
@@ -489,8 +539,5 @@ def reduce(reducer, rhoTS_expansion_gpu, ncols_gpu, nmodes_gpu):
 _reducer = mod.get_function("nv_reduc")
 reduce(_reducer, all_rhots_ylm, ncols_gpu, nmodes_gpu)
 
-import pdb
-pdb.set_trace()
-x = 2
 
 
